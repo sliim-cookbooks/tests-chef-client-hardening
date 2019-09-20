2b5f3b6d150780e0d9d0f9e7453f09a48048867e
Sliim <sliim@mailoo.org>
2019-09-20 11:30:35 +0200

    Check for all files/dirs in /etc/chef directory

--- controls/chef_client_spec.rb
+++ controls/chef_client_spec.rb
@@ -6,18 +6,16 @@ only_if do
   command('chef-client').exist?
 end
 
-%w(client.rb client.pem encrypted_data_bag_secret).each do |file|
+::Dir['/etc/chef/*'].each do |file|
   control "chef-client-#{file}" do
     impact 1.0
     title "Check #{file} owner, group and permissions."
     desc "The #{file} file should owned by root, only be readable by owner."
 
-    describe file("/etc/chef/#{file}") do
-      it { should exist }
-      it { should be_file }
+    describe file(file) do
       it { should be_owned_by 'root' }
       it { should be_grouped_into 'root' }
-      it { should_not be_executable }
+      it { should_not be_executable } if ::File.file?(file)
       it { should be_readable.by('owner') }
       it { should_not be_readable.by('group') }
       it { should_not be_readable.by('other') }
